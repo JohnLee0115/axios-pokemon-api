f92d47acbb81a78abca68d575c8b3cee55550e11
johnlee0115 <johnlee011500@gmail.com>
2023-07-13 15:45:00 -0700

    axios_pokemon_api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
+import axios from 'axios'
+
+
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([])
+
+  const FetchPokemon = (e) => {
+    e.preventDefault();
+
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+    .then(response => {
+      setPokemonList(response.data.results)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={FetchPokemon}>Fetch Pokemon</button>
+      <ul>
+        {pokemonList.map( (pokemon, i) => 
+        <div className='flex'>
+          <li key={i}>{pokemon.name}</li>
+        </div>
+        ) }
+      </ul>
     </div>
   );
 }
